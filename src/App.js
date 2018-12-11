2eaade87cf3704fe2d70a0a0b42922322331169b
Tonisal <markus.bressel@gmx.de>
2018-12-11 07:56:55 +0100

    add current status

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Grid, Jumbotron} from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
+import Results from './components/Results';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Jumbotron>
+                    <Grid>
+                        <h1>Search App</h1>
+                        <p>This is a simple search App</p>
+                        <SearchForm />
+                    </Grid>
+                </Jumbotron>;
+                <Results />
+            </div>
+        );
+    }
 }
 
 export default App;
